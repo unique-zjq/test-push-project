a771427125d370ac2d2f283153a3be3f7701335d
unique <Zjq17633545451@163.com>
2023-08-30 20:20:31 +0800

    小红修改

---
+++ src/main/java/com/unique/test/Second.java
@@ -0,0 +1,13 @@
+package com.unique.test;
+
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
+
+/**
+ * @author unique
+ * @version 1.0
+ */
+public class Second {
+    private String saySecond(){
+        return "这是第二次修改";
+    }
+}
