f66f1dccf9870b85042bf343dc2ec84621192e3b
unique <Zjq17633545451@163.com>
2023-08-30 20:01:47 +0800

    第一次创建

---
+++ src/main/java/com/unique/testspringbootautoconfiguration/TestSpringbootAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.unique.testspringbootautoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootAutoConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringbootAutoConfigurationApplication.class, args);
+    }
+
+}
